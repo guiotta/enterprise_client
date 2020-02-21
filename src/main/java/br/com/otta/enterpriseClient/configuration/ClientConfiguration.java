37f4d842015bd06a6265acea7e119eeadd05a0bf
Guilherme <guilhermeotta@gmail.com>
2020-02-21 09:32:25 -0300

    Adiciona pool de conexões para o cliente do Jersey.

--- src/main/java/br/com/otta/enterpriseClient/configuration/ClientConfiguration.java
+++ src/main/java/br/com/otta/enterpriseClient/configuration/ClientConfiguration.java
@@ -4,6 +4,11 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.glassfish.jersey.apache.connector.ApacheClientProperties;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.client.proxy.WebResourceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +27,34 @@ public class ClientConfiguration {
     @Bean
     public Client clientBuilder() {
         return ClientBuilder.newClient();
+    }
+
+    @Bean
+    public ClientConfig clientConfig(PoolingHttpClientConnectionManager connectionManager,
+            ApacheConnectorProvider apacheConnectorProvider) {
+        ClientConfig clientConfig = new ClientConfig();
+        // 2000 milisegundos para o timeout de leitura.
+        clientConfig.property(ClientProperties.READ_TIMEOUT, 2000);
+        // 500 milisegundos para o timeout de conexão.
+        clientConfig.property(ClientProperties.CONNECT_TIMEOUT, 500);
+
+        clientConfig.property(ApacheClientProperties.CONNECTION_MANAGER, connectionManager);
+        clientConfig.connectorProvider(apacheConnectorProvider);
+
+        return clientConfig;
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager connectionManager() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        return connectionManager;
+    }
+
+    @Bean
+    public ApacheConnectorProvider apacheConnectorProvider() {
+        return new ApacheConnectorProvider();
     }
 }
